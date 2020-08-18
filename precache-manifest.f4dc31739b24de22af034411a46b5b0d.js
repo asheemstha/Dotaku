86a093e8a2846861cfb398e5abf015496bd829b1
asheemstha <asheem.shrestha01@gmail.com>
2020-08-18 18:21:02 +0545

    Updates

---
+++ precache-manifest.f4dc31739b24de22af034411a46b5b0d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "167198b38218efd478b4b07e847845e8",
+    "url": "/Dotaku/index.html"
+  },
+  {
+    "revision": "1b07ace100924ae45a0d",
+    "url": "/Dotaku/static/css/main.42d76434.chunk.css"
+  },
+  {
+    "revision": "d879561c6b0086c893dc",
+    "url": "/Dotaku/static/js/2.53e60213.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/Dotaku/static/js/2.53e60213.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b07ace100924ae45a0d",
+    "url": "/Dotaku/static/js/main.00239891.chunk.js"
+  },
+  {
+    "revision": "56a1002733a7481e86b6",
+    "url": "/Dotaku/static/js/runtime-main.0dad3907.js"
+  },
+  {
+    "revision": "8919de98cc57e3b6ea352f0a3ddf9635",
+    "url": "/Dotaku/static/media/GRIFTER™+Bold.8919de98.otf"
+  },
+  {
+    "revision": "1a657ce36ce40e4d96f97329c77178f3",
+    "url": "/Dotaku/static/media/HeroBG1.1a657ce3.jpg"
+  },
+  {
+    "revision": "0930073eae9c4dbebce3d43dc419f7b5",
+    "url": "/Dotaku/static/media/Inter.0930073e.ttf"
+  }
+]);
